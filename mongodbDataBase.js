b6910b698396cce31456f34e425d3d166d567440
Vishwanath R <93586382+VishwaR007@users.noreply.github.com>
2024-01-29 12:16:31 +0530

    Update mongodbDataBase.js
    Changed the data base connection url at the time of hosting.

--- mongodbDataBase.js
+++ mongodbDataBase.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 // Connect mongodb
-mongoose.connect("mongodb://localhost:27017/pokedex-app");
+// mongoose.connect("mongodb://localhost:27017/pokedex-app");
+mongoose.connect(
+  "mongodb+srv://vishwanathramaswamy:hyYInRy7gOlDPutY@cluster0.i5p39au.mongodb.net/"
+);
 
 const connectDb = async () => {
   try {
